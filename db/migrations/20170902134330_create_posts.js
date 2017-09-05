c2f4564341caa72f43ce51a1ebc66c82aa0fd63e
victorlau1 <lau.victor.w@gmail.com>
2017-09-05 10:50:47 -0700

    change nullable, add new fields into posts

--- db/migrations/20170902134330_create_posts.js
+++ db/migrations/20170902134330_create_posts.js
@@ -4,6 +4,7 @@ exports.up = function (knex, Promise) {
     knex.schema.createTableIfNotExists('posts', function (table) {
       table.increments('id').unsigned().primary();
       table.string('position', 100).nullable();
+      table.string('position_url', 100).nullable();
       table.string('description', 500).nullable();
       table.string('notes', 500).nullable();
       table.bigInteger('company_id').unsigned().index().references('id').inTable('companies');
@@ -11,11 +12,11 @@ exports.up = function (knex, Promise) {
     }),
     knex.schema.createTableIfNotExists('location', function (table) {
       table.increments('id').unsigned().primary();
-      table.string('street', 255);
+      table.string('street', 255).nullable();
       table.string('street_2', 255).nullable();
       table.string('city', 255);
       table.string('state', 255).nullable();
-      table.bigInteger('zipcode');
+      table.bigInteger('zipcode').nullable();
       table.string('country', 255);
       table.string('country_iso_code');
     }),
@@ -23,6 +24,8 @@ exports.up = function (knex, Promise) {
       table.increments('id').unsigned().primary();
       table.string('name', 100).index();
       table.string('industry', 100).nullable();
+      table.string('logo_url', 255).nullable();
+      table.string('linkedin_url', 255).nullable();
       table.string('description', 500).nullable();
       table.bigInteger('location_id').unsigned().index().references('id').inTable('location').nullable();
     }),
