56b5e6ca474629ddcace95b4fe82bc9b337ebf3a
Matt Nicolaou <matthewpnicolaou@gmail.com>
2017-09-05 21:38:07 -0700

    jobForm

--- client/src/components/jobForm.jsx
+++ client/src/components/jobForm.jsx
@@ -9,40 +9,77 @@ import RaisedButton from 'material-ui/RaisedButton';
 import DatePicker from 'material-ui/DatePicker';
 import TimePicker from 'material-ui/TimePicker';
 
-class jobForm extends Component {
+var moment = require('moment');
+
+class JobForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       open: false,
       title: '',
       company: '',
-      date: '',
+      date: null,
       status: '',
-      value: ''
+      value: 0
     };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleTitle = this.handleTitle.bind(this);
+    this.handleCompany = this.handleCompany.bind(this);
+    this.handleDate = this.handleDate.bind(this);
+    this.handleStatus = this.handleStatus.bind(this);
+    this.saveJob = this.saveJob.bind(this);
+  }
+
+  handleClick () {
+    this.setState({
+      open: !this.state.open
+    });
   }
 
   handleTitle (e) {
-
+    this.setState({
+      title: e.target.value
+    });
   }
 
   handleCompany (e) {
-
+    this.setState({
+      company: e.target.value
+    });
   }
 
-  saveJob (job) {
-    axios.post('/newJob', {
+  handleStatus (event, index, value) {
+    this.setState({
+      status: value,
+      value: index
+    });
+  }
+
+  saveJob () {
+    axios.post('/card', {
       job: {
         title: this.state.title,
         company: this.state.company,
         date: this.state.date,
         status: this.state.status
       }
-    });
+    })
+      .then(function(response) {
+        this.setState({
+          open: false
+        });
+        console.log('sent to server');
+      })
+      .catch(function(error) {
+        console.log('error', error);
+      });
   }
 
-  handleDate (e) {
-
+  handleDate (e, date) {
+    this.setState({
+      date: date
+    });
   }
 
   render() {
@@ -51,37 +88,41 @@ class jobForm extends Component {
         label="Cancel"
         //primary={true}
         //keyboardFocused={true}
-        //onClick={}
+        onClick={this.handleClick}
       />,
       <FlatButton
         label="Save"
         //primary={true}
         //keyboardFocused={true}
-        //onClick={this.saveJob()}
+        onClick={this.saveJob}
       />
     ];
-    const items = ['What is the status of this application?', 'Interested', 'Applied', 'Interview Scheduled', 'Interviewed'];
-
+    const values = ['What is the status of this application?', 'Interested', 'Applied', 'Interview Scheduled', 'Interviewed'];
+    const items = values.map(function(val, i) { return <MenuItem value={i} key ={i} primaryText = {val} />; });
     return (
       <div>
-        <RaisedButton label='Add a job' />
+        <RaisedButton label='Add a job' onClick={this.handleClick}/>
         <Dialog
           title="What are the details?"
           actions={actions}
           modal={false}
           open={this.state.open}
-          onRequestClose={this.handleSubmit}
+          onRequestClose={this.handleClick}
         >
           <TextField onChange={this.handleTitle} hintText="Job title"/><br/>
           <TextField onChange={this.handleCompany} hintText="Company"/><br/>
-          <DatePicker onChange={this.handleDate} value ={this.state.date} hintText="When did you apply?"/><br/>
-          <DropDownMenu maxHeight={300} value={items[0]} onChange={this.handleChange}>
+          <DropDownMenu maxHeight={300} value={this.state.value} onChange={this.handleStatus}>
             {items}
-          </DropDownMenu>
+          </DropDownMenu><br/>
+          <DatePicker onChange={this.handleDate} value ={this.state.date} hintText="When did you apply?"/><br/>
         </Dialog>
       </div>
     );
   }
 }
 
-export default jobForm;
+// function mapStateToProps () {
+  
+// }
+
+export default JobForm;
