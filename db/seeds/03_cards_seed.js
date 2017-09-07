35606d14929620df974255750c3302b13f2c9520
Ting Wang <twang1225@gmail.com>
2017-09-06 20:15:11 -0700

    update cards seed file

--- db/seeds/03_cards_seed.js
+++ db/seeds/03_cards_seed.js
@@ -11,33 +11,25 @@ exports.seed = function(knex, Promise) {
           position_url: 'https://boards.greenhouse.io/vlocity/jobs/781103',
           description: 'Vlocity is a leading industry cloud software company that delivers award winning industry-specific applications on the Salesforce platform.',
           notes: null,
-          company_id: 1,
+          company_id: 12,
           user_id: 1
         }
-        // { id: 2,
-        //   position: '',
-        //   position_url: '',
-        //   description: '',
-        //   notes: '',
-        //   company_id: '',
-        //   user_id: ''
-        // },
-        // { id: 3,
-        //   position: '',
-        //   position_url: '',
-        //   description: '',
-        //   notes: '',
-        //   company_id: '',
-        //   user_id: ''
-        // },
-        // { id: 4,
-        //   position: '',
-        //   position_url: '',
-        //   description: '',
-        //   notes: '',
-        //   company_id: '',
-        //   user_id: ''
-        // }
+        { id: 2,
+          position: 'Front End Developer',
+          position_url: 'https://www.linkedin.com/jobs/view/430047268/',
+          description: 'RHLA Technology is a leading provider of technology professionals on a project and full-time basis.',
+          notes: 'Recruiter reached out to me.',
+          company_id: 3,
+          user_id: 1
+        },
+        { id: 3,
+          position: 'Front End UI Developer',
+          position_url: ''https://www.linkedin.com/jobs/view/380992731/'',
+          description: 'Ladeeda is the leader in cloud security. ',
+          notes: null,
+          company_id: 2,
+          user_id: 1
+        }
       ]);
     });
 };
