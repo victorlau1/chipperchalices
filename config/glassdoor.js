065cbf9c232f6b2570dc36a2c1b91a26e80b0bdb
Matt Nicolaou <matthewpnicolaou@gmail.com>
2017-09-08 14:52:28 -0700

    glassdoor api

---
+++ config/glassdoor.js
@@ -0,0 +1,6 @@
+glassdoorAPI = {
+  partnerId: '193181',
+  key: 'gOyCG4NPIea'
+};
+
+module.exports.config = glassdoorAPI;
--- server/controllers/companies.js
+++ server/controllers/companies.js
@@ -1,8 +1,15 @@
 const models = require('../../db/models');
 const card = require ('./cards.js');
 const lifecycle = require ('./lifecycle.js');
+const config = require ('../../config/glassdoor.js');
+const Glassdoor = require('node-glassdoor').initGlassdoor({
+  partnerId: config.config.partnerId,
+  partnerKey: config.config.key
+});
+
 
 models.Company.findOrCreate = function(req, res) {
+  var isNew = false;
   var cloned = new models.Company({        
     name: req.body.job.company,
     industry: null,
@@ -13,7 +20,17 @@ models.Company.findOrCreate = function(req, res) {
   });
   return cloned.fetch()
     .then(function(result, err) {
-      if (result === null){  console.log('hello'); return cloned.save(); }
+      if (result === null) { 
+        isNew = true;
+        return cloned.save(); 
+      }
+      return result;
+    })
+    .then(result => {
+      if (isNew) {
+        console.log('going to glassdoor');
+        return models.Company.getGlassdoorInfo(result);
+      }
       return result;
     })
     .then(result => {
@@ -22,6 +39,26 @@ models.Company.findOrCreate = function(req, res) {
     })
     .catch(err => {
       res.status(500).send(err);
+    });
+};
+
+models.Company.getGlassdoorInfo = function (company) {
+  console.log(company);
+  Glassdoor.findOneCompany(company.attributes.name, 
+    {
+      country: 'US'
+    })
+    .then(function (data) {
+      console.log('success', data);
+      return company.save ({          
+        industry: data.industryName,
+        logo_url: data.squareLogo,
+        linkedin_url: data.website,
+        description: data.featuredReview.headline
+      });
+    })
+    .catch(function (err) {
+      console.error(err);
     });
 };
 
