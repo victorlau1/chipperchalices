9201eadc066030af87b962ea332fb7021053eaa4
Ting Wang <twang1225@gmail.com>
2017-09-08 19:29:45 -0700

    update company model

--- db/models/companies.js
+++ db/models/companies.js
@@ -4,6 +4,9 @@ const Company = db.Model.extend({
   tableName: 'companies',
   location: function() {
     return this.hasOne('Location');
+  },
+  card: function() {
+    return this.hasMany('Card');
   }
 });
 
