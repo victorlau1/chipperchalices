b095315b776c7ee1e4de579d8098f41a4d226223
Rebecca Chen <flyylikeaduck@gmail.com>
2017-09-21 21:55:59 -0700

    Add user info to header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { Menu, Dropdown, Button, Image } from 'semantic-ui-react';
+import { Menu, Dropdown, Button, Image, Label } from 'semantic-ui-react';
 
 import JobForm from './jobForm.jsx';
 
 // retain named export to test unconnected component
 export class Header extends Component {
+
+  renderLogout () {
+    return (
+      <Menu.Item name='logout'>
+        <a href="/logout">
+          <Button basic inverted color='teal'>
+            Logout
+          </Button>
+        </a>
+      </Menu.Item>
+    );
+  }
 
   render() {
     const { display, photo_url } = this.props;
@@ -14,11 +26,18 @@ export class Header extends Component {
     return (
       <Menu fixed='top' size='large' inverted>
         <Menu.Item header>
-        <Link to='/'><Image src='https://i.imgur.com/IFOyk8I.png?1' /></Link>
+          <Link to='/'>
+            <Image src='https://i.imgur.com/IFOyk8I.png?1' />
+          </Link>
         </Menu.Item>
         <Menu.Item><JobForm /></Menu.Item>
         <Menu.Item><Button basic inverted color='teal'>Insights</Button></Menu.Item>
-        <Menu.Item position='right'><a href="/logout"><Button basic inverted color='teal'>Logout</Button></a></Menu.Item>
+
+        <Menu.Menu position='right'>
+          <Menu.Item>{display}</Menu.Item>
+          <Image src={photo_url} size='tiny' shape='circular'/>
+          {this.renderLogout()}
+        </Menu.Menu>
       </Menu>
     );
   }
