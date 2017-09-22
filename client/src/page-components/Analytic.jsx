99b14b7660e208026432173a6cfe76259e391cf1
victorlau1 <lau.victor.w@gmail.com>
2017-09-21 20:23:43 -0700

    added grid

--- client/src/page-components/Analytic.jsx
+++ client/src/page-components/Analytic.jsx
@@ -4,6 +4,7 @@ import JobChart from '../containers/JobChart.jsx';
 import CompanyChart from '../containers/CompanyChart.jsx';
 import StatusChart from '../containers/StatusChart.jsx';
 import CycleChart from '../containers/CycleChart.jsx';
+import {Grid} from 'semantic-ui-react';
 
 class Analytic extends Component {
   
@@ -12,17 +13,30 @@ class Analytic extends Component {
       <div>
         <br/>
         <br/>
-        <br/>
-        <h1>Total User Cards</h1>
-        <UserChart/>
-        <h1>Total Jobs Cards </h1>
-        <JobChart/>
-        <h1>Total Companies </h1>
-        <CompanyChart/>
-        <h1>Total Statuses</h1>
-        <StatusChart/>
-        <h1>Cycle Changes</h1>
-        <CycleChart/>
+        <Grid centered padded='horizontally'>
+          <Grid.Column width={12} >
+            <Grid.Row>
+              <h1 align="center">Total User Cards</h1>
+              <UserChart/>
+            </Grid.Row>
+            <Grid.Row>
+              <h1 align="center">Total Jobs Cards </h1>
+              <JobChart/>
+            </Grid.Row>
+            <Grid.Row>
+              <h1 align="center">Total Companies </h1>
+              <CompanyChart/>
+            </Grid.Row>
+            <Grid.Row>  
+              <h1 align="center">Total Statuses</h1>
+              <StatusChart/>
+            </Grid.Row>
+            <Grid.Row>
+              <h1 align="center">Cycle Changes</h1>
+              <CycleChart/>
+            </Grid.Row>  
+          </Grid.Column>
+        </Grid>
       </div>
     );
   }
